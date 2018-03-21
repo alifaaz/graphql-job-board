273cbfc350cbcd26b47c15bc765dd3ffe13087cb
Mirko Nasato <mirko@up2skill.com>
2018-03-21 13:59:28 +0000

    server: createJob requires user auth

--- server/server.js
+++ server/server.js
@@ -21,7 +21,10 @@ app.use(cors(), bodyParser.json(), expressJwt({
   credentialsRequired: false
 }));
 
-app.use('/graphql', graphqlExpress({schema}));
+app.use('/graphql', graphqlExpress((req) => ({
+  schema,
+  context: {user: req.user}
+})));
 app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
 app.post('/login', (req, res) => {
